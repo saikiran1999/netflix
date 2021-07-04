b6e17fb7f91cb0cdacc7c5274c1e7c24de21e3f0
saikiran1999 <sannibabuvet@gmail.com>
2021-07-04 12:52:34 +0530

    homepage component added

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDgRnS2ZByGU7aXtkwXB_xGkDMya1-S9kY",
+    authDomain: "netflix-clone-6e4b2.firebaseapp.com",
+    projectId: "netflix-clone-6e4b2",
+    storageBucket: "netflix-clone-6e4b2.appspot.com",
+    messagingSenderId: "493165648506",
+    appId: "1:493165648506:web:cedcfe9241f4480cc339c3",
+    measurementId: "G-ZLL5284X21"
+  };
